cd59f69d6c7231599e0671d7ceb3b1299d3bb9bc
sorrowlol <sorrowlol@outlook.com>
2022-06-13 02:41:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Building my own library using JS.
+
+The idea is to add books throughout my programming journey that seem interesting to read. This way I can always
+reference this library when looking for new topics to read, whether it be economics, programming or any other topic.
---
+++ public/script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+const displayBooks = (book) => {
+  // Container for each book
+  let bookContainerDiv = document.createElement("div");
+
+};
